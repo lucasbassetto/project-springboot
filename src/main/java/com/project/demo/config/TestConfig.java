f92512de9441ff292faccdd8a51b0ec21542e1f0
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-03-30 14:51:26 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/project/demo/config/TestConfig.java
@@ -0,0 +1,29 @@
+// Essa classe serve para fazer o Data Base "seeding", ou seja, para "popular o banco de dados com alguns objetos".
+
+package com.project.demo.config;
+
+import com.project.demo.entities.User;
+import com.project.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
---
+++ src/main/java/com/project/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+// Classe que acessa o banco de dados e salva os dados.
+
+package com.project.demo.repository;
+
+import com.project.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
