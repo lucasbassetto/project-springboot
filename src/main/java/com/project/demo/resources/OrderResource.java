f86e07323aa83cd08a69ee6dbb747d23170f4d3d
Lucas Bassetto <lucasfbassetto@gmail.com>
2023-04-07 19:58:04 -0300

    User insert

--- src/main/java/com/project/demo/resources/OrderResource.java
+++ src/main/java/com/project/demo/resources/OrderResource.java
@@ -1,14 +1,12 @@
 package com.project.demo.resources;
 
 import com.project.demo.entities.Order;
+import com.project.demo.entities.User;
 import com.project.demo.service.OrderService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
